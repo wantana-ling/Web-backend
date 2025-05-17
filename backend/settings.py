190d7f9cbe76ee19080ea5503cd64e1f94af0565
TonTon888 <wantana.ling@gmail.com>
2025-05-17 08:59:30 +0700

    3

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%*9@36yl*=s!w@c(74w8yu^d8^zzir09oas@x2pzoeyrhkl^%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
