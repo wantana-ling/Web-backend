a9c8c42601f91e6b6c81c9c03fd4d9f92ac2460a
TonTon888 <wantana.ling@gmail.com>
2025-05-17 09:24:40 +0700

    b

--- backend/settings.py
+++ backend/settings.py
@@ -80,13 +80,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'microfinance_db',   
-        'USER': 'admin',            
-        'PASSWORD': 'GC2pmkd7lmRDjLw5nRHiTgcWPcZhPLbH',  
-        'HOST': 'dpg-d0jueu3uibrs73923g80-a.region-postgres.render.com',  
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
